e376c1d1266cf132c6fec87e9b9ac67ca6b40d91
markkod <mark.kodar@gmail.com>
2019-03-19 22:20:40 +0200

    all specs fulfilled, homework done

--- lecture_1/src/index.js
+++ lecture_1/src/index.js
@@ -1,7 +1,7 @@
 import Metronome from './Metronome';
 import '../css/index.css';
 
-const metronome = new Metronome(0);
+const metronome = new Metronome();
 
 document.body.innerHTML = '<h1>The Human Metronome Project</h1>';
 const button = document.createElement('Button');
@@ -9,8 +9,5 @@ button.innerHTML = 'Start metronome';
 button.onclick = function() {
   button.innerHTML = metronome.createTimeDiff() + '; ' + metronome.getCount();
 };
-// const text = document.createTextNode(metronome.createTimeDiff() + '; ' + metronome.getCount());
 
 document.body.appendChild(button);
-
-// window.robot = robot;
--- lecture_1/test/MetronomeTest.js
+++ lecture_1/test/MetronomeTest.js
@@ -1,9 +1,8 @@
 import Metronome from '../src/Metronome';
-import {expect} from '../test/TestHelper';
 import {describe} from 'mocha';
 
 // https://codeburst.io/javascript-unit-testing-using-mocha-and-chai-1d97d9f18e71
-
+/* global expect */
 
 describe('getCount', () => {
   it('should increase the count of the metronome', () => {
